048a62fec0b8436405d92d6f81918bac36b3815a
Alissa Martini <mmrtini4@memphis.edu>
2024-02-07 19:44:19 -0600

    I added the max limit of guesses

--- guess_number.py
+++ guess_number.py
@@ -1,22 +1,29 @@
 number = 10
-print("Enter 'q' to quit")
+max = 7  
+current_guess = 0  
+
+print("Enter 'q' to quit ")
 print("I'm thinking of a number...")
 
-while True:
-    guess = input("What number am I thinking of? ")
-
+while current_guess < max:
+    guess = input("What number am I thinking of?")
+    
     if guess.lower() == 'q':
         print(f"The number was {number}.")
         break
     
-    try:
-        guess = int(guess)
-    except ValueError:
-        print("Invalid input. Please enter a number or 'q' to quit.")
-        continue
+    
+    guess = int(guess)
+    current_guess += 1
     
     if guess == number:
         print("Congratulations! You guessed the right number.")
         break
+    elif guess < number:
+        print("That's too low.")
     else:
-        print("Sorry! That's incorrect. Try again.")
+        print("That's too high.")
+
+if current_guess >= max:
+    print("You've ran out of guesses! The number was" ,{number})
+
